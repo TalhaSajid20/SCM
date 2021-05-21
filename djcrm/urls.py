c590bc1acdea65618ab855ba4302670e6599c94e
TalhaSajid20 <56575482+TalhaSajid20@users.noreply.github.com>
2021-05-21 12:31:09 +0500

    Update urls.py

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,8 +1,16 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from django.urls import path, include
-from SCM.views import landing_page, LandingPageView
+from SCM.views import landing_page, LandingPageView, SignUpView
 
 
 admin.autodiscover()
@@ -10,7 +18,15 @@ admin.autodiscover()
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing-page'),
-    path('SCM/', include('SCM.urls', namespace="SCM")),   
+    path('SCM/', include('SCM.urls', namespace="SCM")),
+    path('agents/', include('agents.urls', namespace="agents")),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('reset-password/',PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/',PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',PasswordResetConfirmView.as_view(), name='password_reset_confirm'), 
+    path('password-reset-complete/',PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')  
     
 ]
 static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
