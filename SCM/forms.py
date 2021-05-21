cc7cda4bebcdaa120f98458db661c0e43f7c62ff
TalhaSajid20 <56575482+TalhaSajid20@users.noreply.github.com>
2021-05-21 12:27:44 +0500

    Update forms.py

--- SCM/forms.py
+++ SCM/forms.py
@@ -1,6 +1,11 @@
 from django import forms
-from .models import SCM1
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.http import request
+from .models import SCM1, Agent
 
+
+User = get_user_model()
 
 class SCMModelForm(forms.ModelForm):
     class Meta:
@@ -10,6 +15,9 @@ class SCMModelForm(forms.ModelForm):
             'last_name',
             'age',
             'agent',
+            'description',
+            'phone_number',
+            'email',
         )
 
 
@@ -18,3 +26,27 @@ class SCMform(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     age = forms.IntegerField(min_value=0)
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ("username",)
+        field_classes = {'username': UsernameField}
+
+
+class AssignAgentForm(forms.Form):
+    agent = forms.ModelChoiceField(queryset=Agent.objects.none())
+    
+    def __init__(self, *args, **kwargs):
+        request = kwargs.pop("request")
+        agents= Agent.objects.filter(organisation= request.user.userprofile)
+        super(AssignAgentForm,self).__init__(*args,**kwargs)
+        self.fields["agent"].queryset = agents
+
+class SCMCategoryUpdateForm(forms.ModelForm):
+    class Meta:
+        model = SCM1
+        fields = (
+            'category',
+        )
