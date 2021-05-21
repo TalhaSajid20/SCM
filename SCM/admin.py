2e7c6b2844313cc358c56e053472bfb1a45235c7
TalhaSajid20 <56575482+TalhaSajid20@users.noreply.github.com>
2021-05-21 12:27:06 +0500

    Update admin.py

--- SCM/admin.py
+++ SCM/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, SCM1, Agent
+from .models import User, SCM1, Agent, UserProfile, Category
 
+admin.site.register(Category)
 admin.site.register(User)
+admin.site.register(UserProfile)
 admin.site.register(SCM1)
 admin.site.register(Agent)
 
